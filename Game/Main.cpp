c3c9c293816206ee6d8a61b2e1288e544bcb099e
Shiftyeyes654 <60528235+Shiftyeyes654@users.noreply.github.com>
2020-08-03 11:10:53 -0600

    Added Engine Added SDL

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -2,11 +2,31 @@
 //
 
 #include <iostream>
+#include <SDL.h>
 
-int main()
+int main(int, char**)
 {
-    std::cout << "Hello World!\n";
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	SDL_Window* window = SDL_CreateWindow("GAT150", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+	if (window == nullptr) {
+		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	// wait for keyboard enter to exit
+	std::getchar();
+
+	SDL_Quit();
+
+	return 0;
 }
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
